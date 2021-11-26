a5642dfcb2636a1e1085de762387be6c1bc7336e
cristianireyes <cristianir.dev@gmail.com>
2021-11-26 00:54:53 -0300

    feat: Environment variables added

--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
 import express from 'express';
+import { loadAppConfig } from './src/config';
+
+// Setup App config
+loadAppConfig();
+
 const app = express();
-const PORT = 5000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
